efdb2967d587b037779b3aa22ff5e3f90ed3c7af
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-11-29 18:50:32 -0500

    best I can tweak not consolidating banner and body image

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,18 @@
 import { Divider } from '@mui/material';
 import React from 'react'
+import banner from "../Assets/Images/banner.jpg";
 
 const Header = () => {
   return (
-    <header>
-      <h1>navBar Here</h1>
+    <header style={{ 
+      backgroundPosition: '0px 0px',
+      backgroundImage: `url(${banner})`, 
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '55%',
+      height: '25vh',
+      backgroundColor: 'white',
+    }}>
+      <h2>navBar Here</h2>
     </header>
   )
 }
