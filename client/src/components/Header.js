5d89e8ca26172bf5dd10971f3c1304536797c908
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-11-29 17:43:05 -0500

    due to time constraint, banner and body background consolidated on noo repeat

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,10 @@
 import { Divider } from '@mui/material';
 import React from 'react'
-import banner from "../Assets/Images/banner.jpg";
 
 const Header = () => {
   return (
-    <header style={{ 
-      backgroundImage: `url(${banner})`, backgroundSize: 100 
-    }}>
-      <h1 id="test">testing</h1>
+    <header>
+      <h1>navBar Here</h1>
     </header>
   )
 }
