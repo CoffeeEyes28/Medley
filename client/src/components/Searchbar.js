dad578477ee3e05ff21b1c67819f5cdf5375b19a
Greg <litm47@gmail.com>
2022-11-29 12:20:04 -0500

    building searchbar component

---
+++ client/src/components/Searchbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import search from '../utils/API';
+
+
+
+
+const Searchbar = () => {
+    return (
+        <form></form>
+    )
+}
+
+
+export default Searchbar;
+
+
+
