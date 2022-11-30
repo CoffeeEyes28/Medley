5e3e608c66a29a10737510b32da3da9f6b76d333
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-11-29 19:57:27 -0500

    worked on profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,63 +1,143 @@
+// import React from 'react';
+// import { Navigate, useParams } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+
+
+// import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+// import Auth from '../utils/auth';
+
+// const Profile = () => {
+//   const { username: userParam } = useParams();
+
+//   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+//     variables: { username: userParam },
+//   });
+
+//   const user = data?.me || data?.user || {};
+//   // navigate to personal profile page if username is yours
+//   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+//     return <Navigate to="/me" />;
+//   }
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (!user?.username) {
+//     return (
+//       <h4>
+//         You need to be logged in to see this. Use the navigation links above to
+//         sign up or log in!
+//       </h4>
+//     );
+//   }
+//   return (
+//     <div>
+//       <div className="flex-row justify-center mb-3">
+//         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+//           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+//         </h2>
+
+//         <div className="col-12 col-md-10 mb-5">
+//           <ThoughtList
+//             thoughts={user.thoughts}
+//             title={`${user.username}'s thoughts...`}
+//             showTitle={false}
+//             showUsername={false}
+//           />
+//         </div>
+//         {!userParam && (
+//           <div
+//             className="col-12 col-md-10 mb-3 p-3"
+//             style={{ border: '1px dotted #1a1a1a' }}
+//           >
+//             <ThoughtForm />
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-
-
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_RECORD } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+// import { removeBookId } from '../utils/localStorage';
 
-const Profile = () => {
-  const { username: userParam } = useParams();
+const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [removeRecord, {error} ] = useMutation(REMOVE_RECORD);
+  const userData = data?.me || [];
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
+  
 
-  const user = data?.me || data?.user || {};
-  // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/me" />;
-  }
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
+    if (!token) {
+      return false;
+    }
+console.log(bookId)
+    try {
+      let data = await removeRecord({
+        variables:{ bookId: bookId},
+      });
+console.log(data)
+      // setUserData(updatedUser);
+      // upon success, remove book's id from localStorage
+      // removeBookId(bookId);
+    } catch (err) {
+      console.error(error);
+    }
+  };
+
+  // if data isn't here yet, say so
   if (loading) {
-    return <div>Loading...</div>;
+    return <h2>LOADING...</h2>;
   }
 
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
-  }
   return (
-    <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
         </h2>
-
-        <div className="col-12 col-md-10 mb-5">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-            showTitle={false}
-            showUsername={false}
-          />
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <ThoughtForm />
-          </div>
-        )}
-      </div>
-    </div>
+        <CardColumns>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
   );
 };
 
-export default Profile;
+export default SavedBooks;
+
+
+// export default Profile;
