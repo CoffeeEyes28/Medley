bfbbfb15111f4e3b37eb70d6b40dc482e317f608
Monica <monicadolcemascolo@gmail.com>
2022-11-30 18:03:54 -0500

    editing aside

--- client/src/components/Aside.js
+++ client/src/components/Aside.js
@@ -1,43 +1,24 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-
-import { search } from '../utils/API';
-
+import React from "react";
+import { useState } from "react";
+import { useQuery } from '@apollo/client';
 import { GET_All_USERS } from '../utils/queries';
-import { SAVE_RECORD } from '../utils/mutations';
-
-import Auth from '../utils/auth';
-
-import { Divider } from '@mui/material';
-
-import Searchbar from '../components/Searchbar';
-import banner from "../Assets/Images/banner.jpg";
 
 
 
 
-const SearchRecords = () => {
-const [searchedRecords, setSearchedRecords] = useState([]);
-const [searchInput, setSearchInput] = useState('');
 
-const [savedRecordIds, setSavedRecordIds] = useState(getSavedRecordIds());
 
-const [saveRecord] = useMutation(SAVE_RECORD);
 
-useEffect(() => {
+const Aside = () => {
 
-    return () => saveRecordIds(savedRecordIds);
-});
+    const { data } = useQuery(GET_All_USERS) 
 
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    if(!searchInput) {
-        return
-    }
+    return (
+        <div>
+            <h1>Render</h1>
+            <h1>{data}</h1>
+        </div>
+    )
 }
 
-
-
-
-}
+export default Aside;
