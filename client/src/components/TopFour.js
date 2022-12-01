822556faa0d297cff831f11883ef448eb2ed655e
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-11-30 21:47:19 -0500

    quick and dirty shortcut works but want to explore further

--- client/src/components/TopFour.js
+++ client/src/components/TopFour.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import AlbumArt from '../components/AlbumArt';
+import { SAVE_TOP } from '../utils/mutations';
+import { UPDATE_TOP } from '../utils/mutations';
 
-const TopFour = () => {
-    const { loading, data } = useQuery(GET_ME); 
-    const userData = data?.me || [];
-    console.log(userData)
-    return (
-    <div>
-        {userData.medley.map((four, i) => {
-            if (i <= 3){
-                return <AlbumArt image = {four.image} />
-            }
-        })}
-    </div>
-  )
-}
+
 
 export default TopFour;
+
+// The below works for sure but it's consolidated. testing for saveTop and updateTop by delving in
+//  const TopFour = () => {
+//     const { loading, data } = useQuery(GET_ME); 
+//     const userData = data?.me || [];
+//     console.log(userData)
+//     return (
+//     <div>
+//         {userData.medley.map((four, i) => {
+//             if (i <= 3){
+//                 return <AlbumArt image = {four.image} />
+//             }
+//         })}
+//     </div>
+//   )
+// }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,15 @@
 // placeholder
 
-import { Divider } from '@mui/material';
+
 import React from 'react'
-import banner from "../Assets/Images/banner.jpg";
-import Searchbar from '../components/Searchbar';
 import TopFour from '../components/TopFour';
 
 const Profile = () => {
     return (
         <div>
             <br></br>
-            <br></br>
-            <Searchbar />
-            <br></br>
             <h1>Top Four from My Medley</h1>
             <TopFour />
-            <br></br>
             <br></br>
         </div>
 
