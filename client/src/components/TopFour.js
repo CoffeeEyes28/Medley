97db596e238324694315c178a5940ed23827e093
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-11-30 22:19:21 -0500

    returned to previous quick shortcut until I can figure out how to create individual cards and set items even after reloading without saveTop and updateTop

--- client/src/components/TopFour.js
+++ client/src/components/TopFour.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { useQuery, useMutation } from '@apollo/client';
+// import {  Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
+// import { SAVE_TOP } from '../utils/mutations';
+// import { UPDATE_TOP } from '../utils/mutations';
+// import Auth from '../utils/auth';
 import AlbumArt from '../components/AlbumArt';
-import { SAVE_TOP } from '../utils/mutations';
-import { UPDATE_TOP } from '../utils/mutations';
 
-
+const TopFour = () => {
+    const { loading, data } = useQuery(GET_ME); 
+    const userData = data?.me || [];
+    console.log(userData)
+    return (
+    <div>
+        {userData.medley.map((four, i) => {
+            if (i <= 3){
+                return <AlbumArt image = {four.image} />
+            }
+        })}
+    </div>
+  )
+}
 
 export default TopFour;
-
-// The below works for sure but it's consolidated. testing for saveTop and updateTop by delving in
-//  const TopFour = () => {
-//     const { loading, data } = useQuery(GET_ME); 
-//     const userData = data?.me || [];
-//     console.log(userData)
-//     return (
-//     <div>
-//         {userData.medley.map((four, i) => {
-//             if (i <= 3){
-//                 return <AlbumArt image = {four.image} />
-//             }
-//         })}
-//     </div>
-//   )
-// }
