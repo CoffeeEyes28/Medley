90ddd8c63f7b10790b88aa19b90cf56194b28e2c
Greg <litm47@gmail.com>
2022-12-02 14:20:12 -0500

    fixed bug where save top four wouldnt render for new accounts

--- client/src/components/Medley.js
+++ client/src/components/Medley.js
@@ -111,9 +111,9 @@ const Medley = ({allowDelete, userData}) => {
                     {allowDelete && !userData.topFour.some((record => record.album_name === medley.album_name)) && (<Button className='btn-block btn-danger' onClick={() => handleRemoveRecord(medley._id)}>
                       Delete this Album!
                     </Button>)}
-                    {userData.topFour.length<5 || userData.topFour.some((record => record.album_name === medley.album_name)) ? '' : ( <Button disabled={userData.topFour.some((record => record.album_name === medley.album_name))}className='btn-block btn-danger' onClick={() => handleSaveTop(medley)}>
+                    {userData.topFour.length<5  &&  ( <Button hidden={userData.topFour.length>=4 || userData.topFour.some((record => record.album_name === medley.album_name))}className='btn-block btn-danger' onClick={() => handleSaveTop(medley)}>
   Save to topFour!
- </Button>)}
+ </Button>) }
 
                   </Card.Body>
                 </item>
