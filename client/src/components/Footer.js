04e7a677bb829a493198e3a1ccd0ed4bf07eada3
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-12-02 21:57:56 -0500

    medley.js integrated material ui created cards and background, centered footer.js text

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+// import {icon}  from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+// import LastFM from '../Assets/Images/lastFMlogo.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <footer class="small bg-light ">
+      <div class="container py-3 py-sm-4">
+        <div class="row justify-content-md-center py-2">
+          <div class="col-md-auto">
+           <a href="https://github.com/CoffeeEyes28/Medley"><GitHubIcon /></a>
+          </div>
+          <div class="col-md-auto">
+            <a href="https://twitter.com/"><TwitterIcon /></a>
+          </div>
+          <div class="col-md-auto">
+            <a href="https://www.facebook.com/"><FacebookIcon /></a>
+          </div>
+        </div>
+        <div class="row justify-content-md-center py-2">
+          <div class="col-md-auto">
+            <ul class="list-inline">
+              <li class="list-inline-item">&copy; 2022 Created By <CopyrightIcon />ampNerd, Inc.</li>
+              <li class="list-inline-item">All rights reserved.</li>
+              <br></br>
+              <div class="row justify-content-md-center py-2">
+              <div class="col-md-auto">
+              <li>Powered By LastFM</li>
+              </div>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- client/src/components/Medley.js
+++ client/src/components/Medley.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
-import { Container, Card, Button } from 'react-bootstrap';
+// import { Card } from 'react-bootstrap';
 import { useMutation, useQuery } from '@apollo/client';
 import { GET_USER, GET_ME } from '../utils/queries';
 import { REMOVE_RECORD } from '../utils/mutations';
 import Auth from '../utils/auth';
 // import AlbumArt from '../components/AlbumArt';
-import { SAVE_TOP} from '../utils/mutations';
+import { SAVE_TOP } from '../utils/mutations';
 
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CardContent, CardMedia, Typography } from '@mui/material';
+import { color } from '@mui/system';
+import { red } from '@mui/material/colors';
+import Grid from '@mui/material/Grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Delete from '@mui/icons-material/Delete';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+
+
+
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -23,13 +41,13 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 
-const Medley = ({allowDelete, userData}) => {
+const Medley = ({ allowDelete, userData }) => {
 
 
 
 
   const [removeRecord, { error }] = useMutation(REMOVE_RECORD);
-  const [saveTop, {err}] = useMutation(SAVE_TOP);
+  const [saveTop, { err }] = useMutation(SAVE_TOP);
 
 
 
@@ -58,26 +76,26 @@ const Medley = ({allowDelete, userData}) => {
 
     if (!token) {
       return false;
-  }
-  console.log(input)
-  const frank = { 
-    artist: input.artist, album_name: input.album_name, 
-    image: input.image
     }
-  try {
+    console.log(input)
+    const frank = {
+      artist: input.artist, album_name: input.album_name,
+      image: input.image
+    }
+    try {
       let data = await saveTop({
-          variables: {input:frank},
+        variables: { input: frank },
       });
       console.log(data)
 
-  } catch (err) {
+    } catch (err) {
       console.error(error);
-  }
+    }
 
 
   }
 
-  
+
 
   if (!userData?.username) {
     return (
@@ -89,38 +107,57 @@ const Medley = ({allowDelete, userData}) => {
   }
 
   return (
-    <div>
+    <React.Fragment>
 
+      <CssBaseline />
+      <Box sx={{
+            pt: 9,
+            pb: 9,
+          }}>
+      <Container  maxWidth="md">
+      <Grid   sx={{
+            bgcolor: 'background.paper',
+            pt: 3,
+            pl:3,
+            pr:3,
+            pb: 3,
+          }} container spacing={4}>
+        <Box>
+          <Typography component="h1"
+            variant="h4"
+            align="end"
+            color="text.primary"
+            gutterBottom>
 
-      <Container>
-        <div className="flex-row justify-center mb-3">
-          {/* <h2>
             {userData.medley.length
-              ? `Viewing ${userData.medley.length} saved ${userData.medley.length === 1 ? 'medley' : 'medleys'}:`
-              : 'You have no saved medleys!'}
-          </h2> */}
-         
-          <Stack direction="row" spacing={2}>
+              ? `Records: ${userData.medley.length} in ${userData.medley.length === 1 ? 'Medley' : 'Medleys'}`
+              : 'You have no saved Medleys!'}
+          </Typography>
+        </Box>
+
+        
+          <ImageList sx={{ pt: 4 }} cols={3} rowHeight={164}>
             {userData.medley.map((medley) => {
               return (
-                <item key={medley._Id} border='dark'>
-                  {medley.image ? <Card.Img src={medley.image} alt={`The cover for ${medley.artist}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{medley.artist}</Card.Title>
-                    <p className='small'>Album: {medley.album_name}</p>
-                    {allowDelete && !userData.topFour.some((record => record.album_name === medley.album_name)) && (<Button className='btn-block btn-danger' onClick={() => handleRemoveRecord(medley._id)}>
-                      Delete this Album!
+                <CardMedia  key={medley._Id} border='dark'>
+                  {medley.image ? <CardMedia component="img" src={medley.image} alt={`The cover for ${medley.artist}`} variant='top' /> : null}
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h3">{medley.artist}</Typography>
+                    <Typography className='small'>Album: {medley.album_name}</Typography>
+                    {allowDelete && !userData.topFour.some((record => record.album_name === medley.album_name)) && (<IconButton aria-label="delete" variant="contained" color="secondary" onClick={() => handleRemoveRecord(medley._id)}>
+                      <RemoveCircleIcon />
+                    </IconButton>)}
+                    {allowDelete && userData.topFour.length < 5 && (<Button variant="contained" size="small" hidden={userData.topFour.length >= 4 || userData.topFour.some((record => record.album_name === medley.album_name))} onClick={() => handleSaveTop(medley)}>
+                      Save to topFour!
                     </Button>)}
-                    {allowDelete && userData.topFour.length<5  &&  ( <Button hidden={userData.topFour.length>=4 || userData.topFour.some((record => record.album_name === medley.album_name))}className='btn-block btn-danger' onClick={() => handleSaveTop(medley)}>
-  Save to topFour!
- </Button>) }
 
-                  </Card.Body>
-                </item>
+                  </CardContent>
+                </CardMedia>
               );
             })}
-          </Stack>
-          {/* {!userParam && (
+          </ImageList>
+        </Grid>
+        {/* {!userParam && (
             <div className="col-12 col-md-10 mb-3 p-3"
               style={{ border: '1px dotted #1a1a1a' }}>
               <Stack direction="row" spacing={2}>
@@ -139,10 +176,11 @@ const Medley = ({allowDelete, userData}) => {
 
             </div>
           )} */}
-        </div>
+
 
       </Container >
-    </div>
+      </Box>
+    </React.Fragment >
 
   )
 }
