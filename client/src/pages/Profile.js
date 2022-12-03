c26f87d2d123884d1164551741e93a9e0dd77da3
Greg <litm47@gmail.com>
2022-12-03 18:33:14 -0500

    fixed profile header and added rection badge

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -11,6 +11,10 @@ import Reaction from '../components/Reaction';
 import Auth from '../utils/auth';
 import { GET_USER, GET_ME } from '../utils/queries';
 
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
 
 
 
@@ -42,11 +46,18 @@ const Profile = ({allowDelete}) => {
       
     return (
         <div>
-             <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-            Viewing {username ? `${userData.username}'s` : 'your'} profile.
-            <Reaction allowDelete={allowDelete} userData={userData} />
-          </h2>
+          
+          < div className="bg-dark text-light p-3 mb-5">
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <Typography variant="h2" compent="h2"> Viewing {username ? `${userData.username}'s` : 'your'} Medley     </Typography>
+            <br></br>
+             <Reaction allowDelete={allowDelete} userData={userData} />
+
+             
+             
            
+             </Box>
+            </div>
             <br></br>
             <br></br>
             <br></br>
