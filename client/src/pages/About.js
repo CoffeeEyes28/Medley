4d844311df9fc07cf3961fad168c023bfbe883d0
Monica <monicadolcemascolo@gmail.com>
2022-12-04 15:08:33 -0500

    fixed position of boxes in about.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import { Box, CardContent, Typography } from '@mui/material';
 import { grey } from "@mui/material/colors";
+
 
 const colorGrey = grey[100];
 
@@ -38,35 +36,42 @@ const card = (
 );
 
 
-
-
-
 function About() {
   return (
     <Box>
-      <Card
-        sx={{
-          backgroundColor: colorGrey,
-          opacity: [0.8],
-        }}
-        variant="outlined">
-        {card}
-      </Card>
+    <Box
+    sx={{
+      backgroundColor: colorGrey,
+      opacity: [0.8],
+      borderRadius: 2,
+      minWidth: '100%',
+      px: 4,
+      py: 1,
+    }}
+    >
 
-      <Box
+        {card}
+        </Box>
+        <Box
         sx={{
-          mt: 15,
-          mb: 30,
-          ml: 50,
-          mr: 50,
-          p: 8,
+          ml: '33%',
+          p: 4,
+          my: 25,
+          maxWidth: '33%',
+          maxHeight: '50%',
+          display: 'flex',
+          flexBasis: 'auto',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          justifyContent: 'center',
           textAlign: 'center',
+          alignItems: 'center',
           backgroundColor: colorGrey,
           opacity: [0.8],
           borderRadius: 2,
         }}
       >
-
+      
         <Typography>Medley is an interactive online music collection that allows users to showcase their taste to others in a quick and engaging manner.</Typography>
 
         <br></br>
